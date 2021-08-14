5b441d690e1f43621339cd16e7fe3f46fb37019b
frozenprocess <54559947+frozenprocess@users.noreply.github.com>
2021-08-14 13:48:07 -0700

    Fix

--- run.sh
+++ run.sh
@@ -1,37 +1,66 @@
 #!/bin/bash
-if ! command -v jq &> /dev/null
+if ! command -v kubectl &> /dev/null
 then
-    echo "jq could not be found"
+    echo "kubectl could not be found"
     exit
 fi
 
-CN="ip-10-0-151-248"
-SN="ip-10-0-170-148"
+CN="ip-10-0-144-18"
+SN="ip-10-0-165-121"
 PREFIX=$(kubectl get installation.operator.tigera.io default -o jsonpath='{.spec.calicoNetwork.linuxDataplane}')
 # MODE=$(kubectl get felixconfiguration default -o jsonpath='{.spec.bpfExternalServiceMode}'
-RULES=$(kubectl get cnp | wc -l)
 MULTI="2"
 PARALLEL="2"
 NAMESPACE="calico-test"
+RULES=$(kubectl get cnp -n $NAMESPACE | wc -l)
+
+# Making sure test namespace exists
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: $NAMESPACE
+EOF
+
+echo "Calico is using $PREFIX dataplane."
+# Run bunch of benchmarks for a better result?
+echo "Running 5 benchmark/s with $RULES rules."
+
 for I in {1..5}
   do
-    echo "Running benchmark $I without any rules"
-    ./knb -cn $CN -sn $SN -m $MULTI -p $PARALLEL -n $NAMESPACE -o data -f "results/$PREFIX-$RULES-RULES-$I.knbdata"
-    echo "Benchmark $I completed. Waiting 60 Seconds for the cleanup ..."
-    sleep 60
+    ./knb -cn $CN -sn $SN -m $MULTI -p $PARALLEL -n $NAMESPACE -o data -f "results/$PREFIX-$RULES-rules-$I.knbdata"
+    echo "Benchmark $I is done, Waiting for the clean up ..."
+    while [ $(kubectl get pods -n $NAMESPACE | wc -l) != "0" ]
+    do
+      sleep 60
+    done
 done
 
-# echo "Importing rules"
-# kubectl apply -f rules
-# sleep 10
+echo "Importing 11 security rules with 100,000 ipsets."
+echo "This will take 20 seconds." # K8s API-server might nag about too many entries at once.
+RULES=`ls rules`
+for entry in $RULES
+do
+  kubectl apply -f rules/$entry
+  sleep 2
+done
 
-# echo "Running benchmark with rules"
-# for I in {1..5}
-#   do
-#     ./knb -cn $CN -sn $SN -m $MULTI -p $PARALLEL -o data -f "results/$PREFIX-with-rules-$I.knbdata"
-#     echo "Benchmark $I completed. Waiting 60 Seconds for the cleanup ..."
-#     sleep 60
-# done
+RULES=$(kubectl get cnp -n $NAMESPACE | wc -l)
 
-# echo "Cleaning up rules"
-# kubectl delete -f rules
+echo "Running 5 benchmark/s with $RULES rules."
+for I in {1..5}
+  do
+    ./knb -cn $CN -sn $SN -m $MULTI -p $PARALLEL -o data -f "results/$PREFIX-$RULES-rules-$I.knbdata"
+    echo "Benchmark $I is done, Waiting for the clean up ..."
+    while [ $(kubectl get pods -n $NAMESPACE | wc -l) != "0" ]
+    do
+      sleep 60
+    done
+done
+
+echo "Cleaning up rules"
+kubectl delete -f rules
+kubectl delete ns $NAMESPACE
+
+# I'm done!
+echo "checkout the results folder!"
