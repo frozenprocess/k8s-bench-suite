a8208af840c1b3eae322f1d0fd4b7e6cd73d4f30
frozenprocess <54559947+frozenprocess@users.noreply.github.com>
2021-08-14 21:29:50 -0700

    Fix

--- apply.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-RULES=`ls /rules`
-for entry in "RULES"/*
-do
-  ./calicoctl apply -f rules/$entry
-  sleep(5)
-done
--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ then
     exit
 fi
 
-CN="ip-10-0-144-18"
-SN="ip-10-0-165-121"
+CN="multistream-worker"
+SN="multistream-worker2"
 PREFIX=$(kubectl get installation.operator.tigera.io default -o jsonpath='{.spec.calicoNetwork.linuxDataplane}')
 # MODE=$(kubectl get felixconfiguration default -o jsonpath='{.spec.bpfExternalServiceMode}'
 MULTI="2"
@@ -24,11 +24,10 @@ EOF
 
 echo "Calico is using $PREFIX dataplane."
 # Run bunch of benchmarks for a better result?
-echo "Running 5 benchmark/s with $RULES rules."
-
 for I in {1..5}
   do
-    ./knb -cn $CN -sn $SN -m $MULTI -p $PARALLEL -n $NAMESPACE -o data -f "results/$PREFIX-$RULES-rules-$I.knbdata"
+    echo "Benchmark $I with $RULES rules."
+    ./knb -v -cn $CN -sn $SN -m $MULTI -P $PARALLEL -n $NAMESPACE -o data -f "results/$PREFIX-$RULES-rules-$I.knbdata"
     echo "Benchmark $I is done, Waiting for the clean up ..."
     while [ $(kubectl get pods -n $NAMESPACE | wc -l) != "0" ]
     do
@@ -36,21 +35,44 @@ for I in {1..5}
     done
 done
 
-echo "Importing 11 security rules with 100,000 ipsets."
-echo "This will take 20 seconds." # K8s API-server might nag about too many entries at once.
-RULES=`ls rules`
-for entry in $RULES
-do
-  kubectl apply -f rules/$entry
-  sleep 2
-done
+echo "Importing 100 security to block 100,000 ips."
+kubectl apply -f rules
+
+kubectl apply -f - <<EOF
+apiVersion: projectcalico.org/v3
+kind: GlobalNetworkPolicy
+metadata:
+  name: allow-egress
+spec:
+  selector: projectcalico.org/namespace == "$NAMESPACE"
+  order: 1000
+  types:
+  - Egress
+  egress:
+  - action: Allow
+---
+apiVersion: projectcalico.org/v3
+kind: GlobalNetworkPolicy
+metadata:
+  name: deny-egress
+spec:
+  selector: projectcalico.org/namespace == "$NAMESPACE"
+  order: 0
+  types:
+  - Egress
+  egress:
+  - action: Deny
+    destination: 
+      selector: IP-deny-list == 'true'
+EOF
 
 RULES=$(kubectl get cnp -n $NAMESPACE | wc -l)
 
 echo "Running 5 benchmark/s with $RULES rules."
 for I in {1..5}
   do
-    ./knb -cn $CN -sn $SN -m $MULTI -p $PARALLEL -n $NAMESPACE -o data -f "results/$PREFIX-$RULES-rules-$I.knbdata"
+    echo "Benchmark $I with $RULES rules."
+    ./knb -v -cn $CN -sn $SN -m $MULTI -P $PARALLEL -n $NAMESPACE -o data -f "results/$PREFIX-$RULES-rules-$I.knbdata"
     echo "Benchmark $I is done, Waiting for the clean up ..."
     while [ $(kubectl get pods -n $NAMESPACE | wc -l) != "0" ]
     do
